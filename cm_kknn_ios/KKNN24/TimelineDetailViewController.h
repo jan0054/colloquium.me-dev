5b6ecb7c1eff8ab8fceecc8b4df90b60e7b50202
Chi-sheng Jan <csjan@tapgo.cc>
2015-05-19 18:23:15 +0800

    timeline updates

---
+++ cm_kknn_ios/KKNN24/TimelineDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  TimelineDetailViewController.h
+//  KKNN24
+//
+//  Created by csjan on 5/19/15.
+//  Copyright (c) 2015 tapgo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TimelineDetailViewController : UIViewController
+
+@end
---
+++ cm_kknn_ios/KKNN24/TimelineViewController.h
@@ -0,0 +1,13 @@
+//
+//  TimelineViewController.h
+//  KKNN24
+//
+//  Created by csjan on 5/19/15.
+//  Copyright (c) 2015 tapgo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TimelineViewController : UIViewController
+
+@end
