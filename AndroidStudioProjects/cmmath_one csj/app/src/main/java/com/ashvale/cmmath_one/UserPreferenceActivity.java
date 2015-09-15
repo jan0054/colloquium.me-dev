95e122983f779f4309ae364bc291d65141092356
Yuehyi <yuehyi@gmail.com>
2015-09-15 19:15:54 +0800

    android user preference

--- AndroidStudioProjects/cmmath_one csj/app/src/main/java/com/ashvale/cmmath_one/UserPreferenceActivity.java
+++ AndroidStudioProjects/cmmath_one csj/app/src/main/java/com/ashvale/cmmath_one/UserPreferenceActivity.java
@@ -21,7 +21,7 @@ import java.util.List;
 import java.util.Set;
 
 
-public class UserPreferenceActivity extends Activity {
+public class UserPreferenceActivity extends BaseActivity {
 
     public ParseUser selfuser;
     public int email_on;
