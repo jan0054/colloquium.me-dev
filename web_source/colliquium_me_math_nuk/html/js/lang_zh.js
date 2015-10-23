3e548a81b408f95f06d6f3e566cb0fef1fffafa0
Yuehyi <yuehyi@gmail.com>
2015-10-23 17:03:45 +0800

    web NUKMATH

---
+++ web_source/colliquium_me_math_nuk/html/js/lang_zh.js
@@ -0,0 +1,79 @@
+// JavaScript Document
+function dw(key) {document.write(key);}
+//--------------------------------------
+var K1 = "2JF8yrgsM5H261Gju4rzKfxFurDZluOfWUq9UnCV";
+var K2 = "xlirVc4ZbYnah6b97WEo4mbOUBvi0RiqZj5GJ9Hi";
+var K3 = "H5mBN0R22C";
+var K4 = "NzNsbc63DG";
+
+var AL_empty = "不能為空白！";
+var BT_add = "新增";
+var BT_addauthor = "新增作者";
+var BT_search = "檢查";
+var BT_upload = "上傳";
+var STR_1 = "* 欄位為必填";
+var STR_address = "地址";
+var STR_author = "作者";
+var STR_capacity = "可容納人數";
+var STR_content = "內容";
+var STR_coordinates = "座標";
+var STR_date = "日期";
+var STR_description = "描述";
+var STR_email = "Email";
+var STR_end = "結束時間";
+var STR_event = "活動";
+var STR_firstname = "名";
+var STR_institution = "服務單位";
+var STR_jobname = "工作名稱";
+var STR_lastname = "姓";
+var STR_location = "教室";
+var STR_note = "註解";
+var STR_pdffile = "PDF 附加檔案";
+var STR_phone = "電話";
+var STR_photo = "照片";
+var STR_name = "名稱"
+var STR_position = "工作職稱";
+var STR_postedby = "Posted by";
+var STR_searchexist = "是否已存在？";
+var STR_session = "領域";
+var STR_start = "開始時間";
+var STR_time = "時間";
+var STR_timeend = "結束時間";
+var STR_timestart = "開始時間";
+var STR_title = "職稱";
+var STR_username = "使用者名稱";
+var STR_webpage = "網頁";
+var STR_website = "網頁";
+var TT_main = "Data Upload";
+var TT_menu = "選單";
+var TT_top = "Top";
+var TT_addabstract = "Add Abstract";
+var TT_addcareer = "新增工作機會";
+var TT_addevent = "新增活動";
+var TT_addlocation = "新增教室";
+var TT_addperson = "新增講者";
+var TT_addposter = "Add Poster";
+var TT_addsession = "新增領域";
+var TT_addtalk = "新增演講";
+var TT_addvenue = "新增場地資訊";
+var TT_instructions = "說明";
+var TT_toptitle = "Colloquium.me 新增資料";
+
+var STR_event_name = "Event Name";
+var STR_event_description = "Event Description";
+var STR_event_startdate = "Start Date";
+var STR_event_enddate = "End Date";
+var STR_event_organizer = "Organizer (institute or person)";
+
+var STR_event_multiple = "Events (hold shift or ctrl to select multiple)";
+
+var OPT_persontitle_professor = "教授(研究員)";
+var OPT_persontitle_postdoc = "博士後";
+var OPT_persontitle_doctor = "博士班學生";
+var OPT_persontitle_master = "碩士班學生";
+var OPT_persontitle_bachelor = "大學部學生";
+var OPT_persontitle_other = "其他";
+
+var STR_2 = "請先新增領域，教室，再新增演講者及演講";
+var STR_3 =	"Go to https://www.parse.com/apps/cm_math_one/collections to view the data ";
+var STR_4 = "(You will need a free account from Parse.com)";
